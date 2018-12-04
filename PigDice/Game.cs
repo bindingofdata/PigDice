91350c9e9cf7129df617755cd16e905ea83871d2
Hunter Curren <hcurren82@hotmail.com>
2018-12-03 17:44:03 -0800

    Add project files.

---
+++ PigDice/Game.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice
+{
+    internal class Game
+    {
+        private readonly Player[] players;
+        private Player currentTurnPlayer;
+        private string gameState;
+        private bool winner;
+        private bool keepPlaying;
+        private int rollingScore;
+
+        public Game ( string player1, string player2 )
+        {
+            players = new Player[] { new Player( player1 ), new Player( player2 ) };
+            keepPlaying = true;
+            NewGame();
+        }
+
+        private string UpdateGameState ()
+        {
+            StringBuilder header = new StringBuilder();
+
+            header.Append( "---=== Pig Dice ===---\n\n" );
+#pragma warning disable RCS1197 // Optimize StringBuilder.Append/AppendLine call.
+            header.Append( $"Name: {players[0].Name} - Score: {players[0].Score}\n" );
+            header.Append( $"Name: {players[1].Name} - Score: {players[1].Score}\n" );
+            header.Append( "---------------------------------------------------\n" );
+#pragma warning restore RCS1197 // Optimize StringBuilder.Append/AppendLine call.
+
+            return header.ToString();
+        }
+
+        public void NewGame ()
+        {
+            while ( keepPlaying )
+            {
+                currentTurnPlayer = players[0];
+                rollingScore = 0;
+                winner = false;
+                keepPlaying = true;
+                gameState = UpdateGameState();
+
+                while ( !winner )
+                {
+                    TakeTurn();
+                    if ( !IsWinner( currentTurnPlayer, out winner ) )
+                        ChangeTurns();
+                } // end current game loop
+                gameState = UpdateGameState();
+
+                Console.Clear();
+
+                Console.WriteLine( gameState );
+                Console.WriteLine( $"{currentTurnPlayer.Name.ToUpper()} IS THE WINNER!" );
+                Console.Write( "press any key to continue..." );
+                Console.ReadLine();
+
+                string userResponse = "";
+                while (userResponse != "y" && userResponse != "n")
+                {
+                    Console.Clear();
+                    Console.WriteLine("---=== Pig Dice ===---\n");
+                    Console.WriteLine(@"Would you like to play again? [y/n]");
+                    Console.Write("-> ");
+                    userResponse = Console.ReadLine().ToLower();
+
+                    if (userResponse != "y" && userResponse != "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("---=== Pig Dice ===---\n");
+                        Console.WriteLine("Invalid entry. Please enter either \"y\" or \"n\".");
+                        Console.Write("press any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else if (userResponse == "n")
+                        keepPlaying = false;
+                }
+            } // end main game loop
+        }
+
+        private bool IsWinner ( Player currentTurnPlayer, out bool playerWon )
+        {
+            return playerWon = currentTurnPlayer.Score >= 100;
+        }
+
+        private void TakeTurn ()
+        {
+            rollingScore = 0;
+            Random die = new Random();
+            string playerInput = "";
+            int firstRoll = 0;
+            int secondRoll = 0;
+            bool rollAgain = true;
+
+            while ( rollAgain )
+            {
+                Console.Clear();
+                PrintTurn(firstRoll, secondRoll);
+
+                Console.WriteLine( "Would you like to roll the dice? [y/n]" );
+                Console.Write( "-> " );
+                playerInput = Console.ReadLine().ToLower();
+
+                if ( playerInput == "n" )
+                {
+                    currentTurnPlayer.Score += rollingScore;
+                    rollAgain = false;
+                }
+                else if (playerInput == "y")
+                {
+                    firstRoll = die.Next( 1, 7 );
+                    secondRoll = die.Next( 1, 7 );
+
+                    rollingScore += firstRoll + secondRoll;
+                    
+                    if ( firstRoll == 1 || secondRoll == 1 )
+                    {
+                        rollAgain = false;
+                        Console.Clear();
+                        Console.WriteLine( "---=== Pig Dice ===---\n" );
+                        Console.WriteLine( "You rolled a 1!" );
+                        Console.WriteLine( $"Die 1: {firstRoll}" );
+                        Console.WriteLine( $"Die 2: {secondRoll}" );
+                        Console.WriteLine( $"Points Lost: {rollingScore}\n" );
+                        Console.Write( "press any key to continue..." );
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine( "---=== Pig Dice ===---\n" );
+                    Console.WriteLine( "Invalid input." );
+                    Console.WriteLine( "Please enter \"y\" for Yes, or \"n\" for No.\n" );
+                    Console.Write( "press any key to continue..." );
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private void PrintTurn (int firstRoll, int secondRoll)
+        {
+            gameState = UpdateGameState();
+
+            StringBuilder turnState = new StringBuilder();
+            turnState.Append( gameState );
+#pragma warning disable RCS1197 // Optimize StringBuilder.Append/AppendLine call.
+            turnState.Append( $"IT'S {currentTurnPlayer.Name.ToUpper()}'S TURN!\n" );
+            turnState.Append( $"Your last roll ({firstRoll}, {secondRoll}) totaled {firstRoll + secondRoll} points.\n" );
+            turnState.Append( $"You have {rollingScore} points so far this turn.\n" );
+#pragma warning restore RCS1197 // Optimize StringBuilder.Append/AppendLine call.
+
+            Console.Write(turnState);
+        }
+
+        private void ChangeTurns ()
+        {
+            if ( currentTurnPlayer == players[0] )
+                currentTurnPlayer = players[1];
+            else
+                currentTurnPlayer = players[0];
+        }
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            string menuSelection = "";
+
+            while(MainMenu(out menuSelection))
+            {
+                // Game exit is hanled in the MainMenu method.
+                switch (menuSelection)
+                {
+                    case "n":
+                    case "1":
+                        NewGame();
+                        break;
+                    case "h":
+                    case "2":
+                    case "?":
+                        DisplayHelp();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static bool MainMenu(out string menuSelection)
+        {
+            bool keepPlaying = true;
+            StringBuilder menu = new StringBuilder();
+
+            menu.Append(header);
+            menu.Append("+ Main Menu +\n");
+            menu.Append("1. [N]ew Game\n");
+            menu.Append("2. [H]ow to play\n");
+            menu.Append("3. [E]xit\n\n");
+            menu.Append("Please make your selection...\n");
+            menu.Append("-> ");
+
+            Console.Clear();
+            Console.Write(menu);
+            menuSelection = Console.ReadLine().ToLower();
+
+            // If the user wants to exit, we do it here before the main switch is hit.
+            if (menuSelection == "e" ||
+                menuSelection == "q" ||
+                menuSelection == "3")
+            {
+                keepPlaying = false;
+            }
+
+            return keepPlaying;
+        }
+
+        private static void NewGame()
+        {
+            bool startGame = false;
+            string player1 = "";
+            string player2 = "";
+
+            while(!startGame)
+            {
+                Console.Clear();
+                Console.Write(header);
+                Console.WriteLine("What is the first player's name? ");
+                Console.Write("-> ");
+                player1 = Console.ReadLine();
+
+                Console.Clear();
+                Console.Write(header);
+                Console.WriteLine("What is the second player's name? ");
+                Console.Write("-> ");
+                player2 = Console.ReadLine();
+
+                Console.Clear();
+                Console.Write(header);
+                Console.WriteLine($"Player 1 = {player1}");
+                Console.WriteLine($"Player 2 = {player2}");
+                Console.WriteLine("Would you like to start the game with these players? [y/n]");
+
+                if (Console.ReadLine().ToLower() == "y")
+                    startGame = true;
+            }
+
+            Game newGame = new Game(player1, player2);
+        }
+
+        private static void DisplayHelp()
+        {
+            StringBuilder help = new StringBuilder();
+
+            help.Append(header);
+            help.Append("Pig Dice is a game played between two people.\n\n");
+            help.Append("On your turn, you can choose one of two actions:\n");
+            help.Append("1. Roll the dice.\n");
+            help.Append("2. End your turn and save your points.\n\n");
+            help.Append("press any key to continue... ");
+            Console.Clear();
+            Console.Write(help);
+            Console.ReadKey();
+
+            help.Clear();
+            help.Append(header);
+            help.Append("You may roll the dice as many times as you wish on your turn.\n");
+            help.Append("However, if either die rolls a 1, your turn ends and you don't get any points.\n\n");
+            help.Append("The first player to successfully save 100 or more points wins!\n\n");
+            help.Append("press any key to continue... ");
+            Console.Clear();
+            Console.Write(help);
+            Console.ReadKey();
+        }
+
+        private const string header = "---=== Pig Dice ===---\n\n";
+    }
+}
